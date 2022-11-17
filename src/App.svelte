f0f03179b028034911cb5968feececb77222b543
Max K <1145793@edu.rut-miit.ru>
2022-11-18 02:06:50 +0300

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<svelte:head>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@500&display=swap" rel="stylesheet">
+    <script crossorigin="anonymous" src="https://kit.fontawesome.com/24814b69dd.js">
+        <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@500&display=swap" rel="stylesheet">
+    </script>
+</svelte:head>
+<script>
+  import FirstContainer from "./lib/firstContainer.svelte";
+  import SecondContainer from "./lib/secondContainer.svelte";
+
+</script>
+
+<main>
+
+    <FirstContainer />
+    <SecondContainer />
+</main>
+
+<style>
+ :root{
+     background: #2a3443;
+     font-family: 'Alata', sans-serif;
+
+ }
+</style>
