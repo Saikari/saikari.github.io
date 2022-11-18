5f11af31ff3beb94a7586a88e3bf6bbfe3511a14
Max K <1145793@edu.rut-miit.ru>
2022-11-18 15:41:59 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'undriven.github.io',
+  base: '/vite-deploy-demo/',
   plugins: [svelte()]
 })
